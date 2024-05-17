09d62113a48ef514c9e751402dbdd3d75a967429
KilianaBldr <kiliana.bldr@gmail.com>
2024-05-17 13:33:45 +0200

    ajout de la verification des champs saisie

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-// Je récupère mes éléments 
-const commentList = document.getElementById('comment-list')
-const commentForm = document.querySelector('form')
-const errorMessage = document.getElementById('error-message')
-const submitButton = document.querySelector('form button')
 
-//Je récupère la valeur de mes champs du formulaire
-const firstName = document.getElementById('first-name').value
-const lastName = document.getElementById('last-name').value
-const message = document.getElementById('message').value
+// je recupere mon formulaire
+let formulaire = document.querySelector("form");
 
-// Ajout d'une actions au clic du button 
-submitButton.addEventListener('click', (event) => {
-//J'évite le chargement par défault
-event.preventDefault()
-//je vérifie si les champs son vides 
-if (firstName === '' || lastName === '' || message === '') {
-    // Et si les champs ou un des champ et vide j'affiche mn message d'erreur
-    errorMessage.style.display = 'block';
-} else {
-    //si tout les champs sont bien remplie alors aucun message d'erreur s'affiche
-    errorMessage.style.display = 'none';
-    //Création du nouvel espace de commentaire
-    const newComment = document.createElement('div');
-    //ajout les champs remplie dans le nouvelle espace commentaire 
-    newComment.innerHTML = ${firstName} ${lastName} ${message};
-    //ajout du nouveau commentaire créer à la liste de commentaire
-    commentList.appendChild(newComment);
-} 
+//Ajout d'une actions au clic du button 
+formulaire.addEventListener("submit", function (event) {
+  event.preventDefault(); 
 });
+
+//je creer une fonction qui control mes champs 
+function controlChamps () {
+    //Je récupère la valeur de mes champs du formulaire
+    let firstName = document.getElementById("first-name").value
+    let lastName = document.getElementById("last-name").value
+    let message = document.getElementById("message").value
+
+
+    //je verifie si les champs sont vides
+        if (firstName === "" || lastName === '' || message === '') {
+            document.getElementById("error-message").style.display = 'block';
+            // Et si oui les champs ou un des champ vide affiche mn message d'erreur
+        } else {
+         //si tout les champs sont bien remplie alors aucun message d'erreur s'affiche
+            document.getElementById("error-message").style.display = 'none';
+        }
+    }
+//fonction pour recuperer les valeurs des champs et pour rajouter le commentaires ecrit 
+
+//reset le fomulaire
+    formulaire.reset();
+
+
